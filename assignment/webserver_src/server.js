b423c702273d968c3241d37366767d1c7b025e29
Eliott Dinfotan <98353358+eliottd7@users.noreply.github.com>
2024-09-18 04:46:58 -0400

    Update server.js

--- assignment/webserver_src/server.js
+++ assignment/webserver_src/server.js
@@ -6,18 +6,25 @@ const { createServer } = require('node:http');
 const server = createServer(app);
 
 const IO = require('socket.io');
+const picoip = "172.31.36.49";
 
 app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+  io.on('connection', (socket) => {
+    var clientip = socket.request.connection.remoteAddress;
+    if (clientip === picoip) {
+      io.emit('pulldata');
+    } else {
+      res.sendFile(join(__dirname, 'index.html'));
+    }
+    socket.on('push', (msg) => {
+      const parts = msg.split(':'); // should be 3 long...
+      io.emit('update_min', parts[0]);
+      io.emit('update_max', parts[1]);
+      io.emit('update_avg', parts[2]);
+    });
+  });
 });
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
-});
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
 });
